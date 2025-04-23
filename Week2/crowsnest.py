1b3410347a4d813aa96c9ceefc4a18a37be6f663
jennmald <jmaldonad@bnl.gov>
2025-04-23 13:42:28 -0400

    add black formatting

--- Week2/crowsnest.py
+++ Week2/crowsnest.py
@@ -10,9 +10,10 @@ def get_args():
 
     parser = argparse.ArgumentParser(
         description="Crow's Nest -- choose the correct article",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('word', metavar='word', help='A word')
+    parser.add_argument("word", metavar="word", help="A word")
 
     return parser.parse_args()
 
@@ -26,13 +27,13 @@ def main():
 
     ## TO DO: Determine a condition to make the article 'a' or 'an'
     ## HINT: update condition to check if the word starts with a consonant
-    article = 'an' if word[0].lower() in 'something' else 'a'
+    article = "an" if word[0].lower() in "something" else "a"
 
     ## TO DO: Update the print statement to use f-string formatting for word and article
     ## HINT: word is {word} and article is {article}
-    print(f'Hello World')
+    print(f"Hello World")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
